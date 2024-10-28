9f48ebd846873e08a484ae3a82c9e7a17c3f648e
ichsan <mchichsannurh@gmail.com>
2024-10-28 12:57:24 +0700

    openclose baru

--- openclose/CinemaCalculations.java
+++ openclose/CinemaCalculations.java
@@ -1,12 +1,9 @@
-class CinemaCalculations {
-    public Double calculateAdminFee(Cinema cinema) {
-    if (cinema instanceof StandardCinema) {
-    return ((StandardCinema) cinema).price * 10 / 100;
-    } else if (cinema instanceof DeluxeCinema) {
-    return ((DeluxeCinema) cinema).price * 12 / 100;
-    } else if (cinema instanceof PremiumCinema) {
-    return ((PremiumCinema) cinema).price * 20 / 100;
-    } else
-    return 0.0;
+class StandardCinema extends Cinema {
+    public StandardCinema(Double price) {
+    this.price = price;
     }
-    }
+    @Override
+Double calculateAdminFee() {
+return price * 10 / 100;
+}
+}
